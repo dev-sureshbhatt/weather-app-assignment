1279c5eb53d3389e8bbceddb30733332771f262d
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-02-11 18:53:02 +0530

    component color change based on temperature done

--- src/components/StateInfo.jsx
+++ src/components/StateInfo.jsx
@@ -4,12 +4,31 @@ import { useContext } from 'react'
 
 export default function StateInfo({value}) {
 
+
+
+  
+
+  // const tempTemp = value.target[1] > 20
+  const Temp = value.target[1]
+  const isHot = Temp <= 20
+  console.log(Temp, isHot)
+
+  // const isHot = 21 > 0
+  // console.log(isHot, "ishot")
+
+
+
   return (
-    
-        <div 
-        className='state-card'>
-          <p className='state-card-country-name'>{value?.target[0]}</p>
-          <p className='state-card-temp-display'>{value?.target[1]}</p>
-        </div>
+
+<>
+    {
+      (isHot) ? (<div style={{backgroundColor: 'green'}}className='state-card'>
+    <p className='state-card-country-name'>{value?.target[0]}</p>
+    <p className='state-card-temp-display'>{value?.target[1]}</p>
+  </div>) : (<div style={{backgroundColor: 'red'}}className='state-card'>
+    <p className='state-card-country-name'>{value?.target[0]}</p>
+    <p className='state-card-temp-display'>{value?.target[1]}</p>
+  </div>)}
+  </>      
   )
 }
