04ea531156870e9f77a9977e5f2f37e9359ffece
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-02-11 13:00:45 +0530

    useEffect implemented

--- src/pages/Graph.jsx
+++ src/pages/Graph.jsx
@@ -1,42 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import '../App.css'
 import StateInfo from '../components/StateInfo'
 
 export default function Graph() {
 
-  
   const [allLocationData, setAllLocationData] = useState([])
 
   const location = ["Chattisgarh","Mumbai","Uttarakhand","Assam","Kerala","Tamilnadu","uttarpradesh","himachal","haryana","punjab","sikkim","rajasthan","bihar","manipur","meghalaya"]
   const getURLData = []
 
-  function testing(){
+  useEffect(()=>{
     try {
 
-      location.forEach((location, index)=>{
-        getURLData[index] = axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=DUZYGZW839GGCYG4GRFT6XQJG&contentType=json`)
-      })
+
+      async function fetchData(){
+        location.forEach((location, index)=>{
+          getURLData[index] = axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=DUZYGZW839GGCYG4GRFT6XQJG&contentType=json`)
+        })
+  
+       axios.all(getURLData).then(
+        axios.spread((...allNewdata)=>{setAllLocationData(allNewdata)})
+       )
+
+      }
 
 
-     axios.all(getURLData).then(
-      axios.spread((...allNewdata)=>{setAllLocationData(allNewdata)})
-     )
+      fetchData()
+
+      
       
 
     } catch (error) {
-      console.log('something went wrong')
+      alert('Something went wrong, please refresh')
     }
 
-    
-  }
+  },[])
+
+
+  
 
 
 
   return (
     <div className='graph-page-wrapper'>
-      <button onClick={testing}>Hi, I am testing button</button>
-      
+         
       {
                     location.map((data)=>
                     <StateInfo value={{location: data}} />
